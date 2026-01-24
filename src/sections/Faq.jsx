24cb9459998ed6e035ad2f3486cd9d8eff41a987
Marshal Ram <84084013+marshaljr@users.noreply.github.com>
2026-01-24 20:43:39 +0545

    Fix typo in FAQ header text

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -10,7 +10,7 @@ const Faq = () => {
         <div className="container pt-28 z-2 relative">
           <div className="flex flex-col mb-24">
             <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
-              Curiosity didn&apos;t kill the cat, it gave it answers.
+              Curiosity didn&apos;t kill the cat, it gave it answer.
             </h3>
             <p className="body-1 max-lg:max-w-sm">
               You&apos;ve got questions, we&apos;ve got answers.
